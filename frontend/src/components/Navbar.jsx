9e89464cb9a0a66fb9ea2cd47970ee185f1b8025
Harsh Choudhary <harshchy2210@gmail.com>
2025-05-25 19:36:49 +0530

    Navbar working state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Sun, Moon, Menu, X, Search } from 'lucide-react';
 import { useState } from 'react';
 import { useUser } from '../contexts/UserContext';
 import Chip from './Chip';
 import Link from 'next/link';
+import styles from './Navbar.module.css'
 
-const Navbar = ()=> {
+const Navbar = () => {
   const { user, logout } = useUser();
   const [theme, setTheme] = useState('light');
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -33,39 +35,38 @@ const Navbar = ()=> {
   };
 
   return (
-    <nav className="navbar">
-      <button className="nav-drawer-button" onClick={toggleSidebar}>
-        <span className="material-icons">
-          {isSidebarOpen ? 'close' : 'menu'}
-        </span>
+    <nav className={styles["navbar"]}>
+      <button className={styles["nav-drawer-button"]} onClick={toggleSidebar}>
+        {isSidebarOpen ? <X className={styles["material-icons"]} /> : <Menu className={styles["material-icons"]} />}
       </button>
-      <div className="nav-main-item">
-        <img src={`/img/logoTitle.png`} className="nav-logo" alt="logoTitle"/>
-        <h2 className="nav-title">HomeMade</h2>
+
+      <div className={styles["nav-main-item"]}>
+        <img src={`/img/logoTitle.png`} className={styles["nav-logo"]} alt="logoTitle" />
+        <h2 className={styles["nav-title"]}>HomeMade</h2>
       </div>
-      <div className="nav-items">
-        
+      <div className={styles["nav-items"]}>
+
         {
-          user && 
-          <div className="search-icon">
-            <span className="material-icons">search</span>
+          user &&
+          <div className={styles["search-icon"]}>
+            <Search className={styles["material-icons"]} />
           </div>
         }
-        <div className="nav-theme" onClick={toggleTheme}>
+        <div className={styles["nav-theme"]} onClick={toggleTheme}>
           {theme === 'light' ? (
-            <span className="material-icons">dark_mode</span>
+            <Moon className={styles["material-icons"]} />
           ) : (
-            <span className="material-icons">light_mode</span>
+            <Sun className={styles["material-icons"]} />
           )}
         </div>
         {user ? (
           <>
-            <div className="nav-user">
-              <span className="nav-circle-letter">
+            <div className={styles["nav-user"]}>
+              <span className={styles["nav-circle-letter"]}>
                 {user.email[0].toUpperCase()}
               </span>
             </div>
-            
+
             <Chip
               label="Logout"
               onClick={handleLogoutClick}
@@ -88,12 +89,12 @@ const Navbar = ()=> {
           </>
         )}
       </div>
-      <div className={`sidebar-items ${isSidebarOpen ? 'show' : 'hide'}`}>
-        <div className="sidebar-item">Item 1</div>
-        <div className="sidebar-item">Item 2</div>
-        <div className="sidebar-item">Item 3</div>
-        <div className="sidebar-item">Item 4</div>
-        <div className="sidebar-item">Item 5</div>
+      <div className={`${styles['sidebar-items']} ${isSidebarOpen ? styles.show : styles.hide}`}>
+        <div className={styles["sidebar-item"]}>Item 1</div>
+        <div className={styles["sidebar-item"]}>Item 2</div>
+        <div className={styles["sidebar-item"]}>Item 3</div>
+        <div className={styles["sidebar-item"]}>Item 4</div>
+        <div className={styles["sidebar-item"]}>Item 5</div>
       </div>
     </nav>
   )
